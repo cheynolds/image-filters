ef66fdf223d4b240bf4ba34c342988deb99728c0
Charlie Romeo <github@cheynolds.com>
2021-05-21 22:37:17 -0700

    Create warhol-filter.py

---
+++ warhol-filter.py
@@ -0,0 +1,52 @@
+"""
+Converts an image to a likeness of 20th century performance artist
+Andy Warhol. 
+"""
+
+from image import SimpleImage
+
+def main():
+  image = simple.image(example.txt)
+  original_height = int(image.width(simple.image))
+  original_width = int(image.heigth(simple.image))
+  
+  #define size of warhol image
+  columns = int(input("How many columns of Warhol would you like?"))
+  rows = int(input("How many rows of Warhol would you like?"))
+  choose_color = str(input("Select a primary color. 'Clear' for standard Warhol")
+ 
+  calculate_warhol(columns, rows, original_height, original_width)
+  create_warhol(height, width)
+  choose_color(color_choice)
+  copy_warhol(start_location, color) 
+  #create warhol canvas
+  #copy image to warhol canvas
+    #apple filter to image pixels
+    
+#calculate size of warhol canvas
+def calculate_warhol(columns, rows, original_height, original_width):
+  pass
+  return(height, width)
+
+#create new blank canvas for filtered warhol images
+def create_warhol(height, width):
+  pass
+
+#calculate color adjustments                     
+def choose_color(color_choice):
+  if color_choice = str('Red'):
+    
+  elif color_choice = str('Orange'):
+  
+  elif color_choice = str('Yellow'):
+  
+  elif color_choice = str('Green'):
+  elif color_choice = str('Blue'):
+  elif color_choice = str('Indigo'):
+  elif color_choice = str('Violet'):
+  
+  return color                   
+                     
+#copy from original image to location on blank canvas
+def copy_warhol(start_location, color):
+  pass
