d9066cb6fdbf83c7ce8079ffc3fa882002931b35
Charlie Romeo <github@cheynolds.com>
2021-05-24 23:57:54 -0700

    Create reflections.py

---
+++ reflections.py
@@ -0,0 +1,5 @@
+"""
+This program takes an image and generates a reflection.
+The top half of the generated image is the same as the original.
+The bottom half is the mirror reflection of the top half.
+"""
